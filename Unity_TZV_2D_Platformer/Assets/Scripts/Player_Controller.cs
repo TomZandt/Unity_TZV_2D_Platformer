d2257d027ef2768b6d64f6557b8509dd8d8bff8a
Tom Zandt-Valentine <tzandt@googlemail.com>
2018-12-01 17:46:30 +0000

    Neatened code.

--- Unity_TZV_2D_Platformer/Assets/Scripts/Player_Controller.cs
+++ Unity_TZV_2D_Platformer/Assets/Scripts/Player_Controller.cs
@@ -14,7 +14,6 @@ public class Player_Controller : MonoBehaviour
     private Player_UserInput userInputObj;
     private Rigidbody2D playerRb2D;
 
-    private Vector2 moveVect = Vector2.zero; // Used for smoothDamp
     private Vector2 velocity = Vector2.zero; // Used for smoothDamp
 
     private const float groundedRadius = 0.02f;
@@ -39,18 +38,21 @@ public class Player_Controller : MonoBehaviour
     //****************************************************************************************************
     private void Update()
     {
-        float currentDirection = userInputObj.getUserInputRawHorizontal();
-
-        // If user has requested right and we are facing left
-        if (currentDirection > 0 && !isFacingRight)
-            FlipPlayer();
-        // If user has requested left and we are facing right
-        else if (currentDirection < 0 && isFacingRight)
-            FlipPlayer();
+        ProcessFlip();
     }
 
     //****************************************************************************************************
     private void FixedUpdate()
+    {
+        CheckForGround();
+
+        ProcessHorizontalMovement();
+
+        ProcessJump();
+    }
+
+    //****************************************************************************************************
+    private void CheckForGround()
     {
         // Assume we are falling
         isGrounded = false;
@@ -62,24 +64,55 @@ public class Player_Controller : MonoBehaviour
         for (int i = 0; i < colliders.Length; i++)
             if (colliders[i].gameObject != gameObject)
                 isGrounded = true;
+    }
 
+    //****************************************************************************************************
+    private void ProcessHorizontalMovement()
+    {
         // Calculate the amount to move the player based on the players movement speed
-        moveVect = new Vector2(userInputObj.getUserInputRawHorizontal() * playerMoveSpeed, playerRb2D.velocity.y);
+        Vector2 moveVect = new Vector2(userInputObj.getUserInputRawHorizontal() * playerMoveSpeed, playerRb2D.velocity.y);
 
         // Smooth the movement and apply it
         playerRb2D.velocity = Vector2.SmoothDamp(playerRb2D.velocity, moveVect, ref velocity, playerMoveSmoothFactor / 100);
+    }
 
+    //****************************************************************************************************
+    private void ProcessJump()
+    {
         // If the user requested jump
         if (userInputObj.getUserInputBoolJump() && isGrounded)
             playerRb2D.AddForce(Vector2.up * playerJumpVelocity * 100f);
 
-        // Add increased gravity and short jump control
-        if (playerRb2D.velocity.y < 0 || playerRb2D.velocity.y > 0 && !userInputObj.getUserInputBoolJump())
-            playerRb2D.velocity += Vector2.up * Physics2D.gravity.y * (playerGravityMultiplier - 1) * Time.fixedDeltaTime;
+        // If the player is falling
+        if (playerRb2D.velocity.y < 0)
+            ApplyNewGravity();
+
+        // If the player is not falling but has let go of jump
+        else if (playerRb2D.velocity.y > 0 && !userInputObj.getUserInputBoolJump())
+            ApplyNewGravity();
     }
 
     //****************************************************************************************************
-    private void FlipPlayer ()
+    private void ApplyNewGravity()
+    {
+        playerRb2D.velocity += Vector2.up * Physics2D.gravity.y * (playerGravityMultiplier - 1) * Time.fixedDeltaTime;
+    }
+
+    //****************************************************************************************************
+    private void ProcessFlip()
+    {
+        float currentDirection = userInputObj.getUserInputRawHorizontal();
+
+        // If user has requested right and we are facing left
+        if (currentDirection > 0 && !isFacingRight)
+            FlipPlayer();
+        // If user has requested left and we are facing right
+        else if (currentDirection < 0 && isFacingRight)
+            FlipPlayer();
+    }
+
+    //****************************************************************************************************
+    private void FlipPlayer()
     {
         // Set direction to opposite
         isFacingRight = !isFacingRight;
