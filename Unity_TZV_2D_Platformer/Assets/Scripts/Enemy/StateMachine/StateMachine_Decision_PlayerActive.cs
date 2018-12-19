b2825f5e4e6a10269f02211ba46286ea9b55deb6
Tom Zandt-Valentine <tzandt@googlemail.com>
2018-12-19 10:37:20 +0000

    Added activePlayer decision
    ref: https://youtu.be/RfWzSRiWR1w?list=PLX2vGYjWbI0ROSj_B0_eir_VkHrEkd4pi

---
+++ Unity_TZV_2D_Platformer/Assets/Scripts/Enemy/StateMachine/StateMachine_Decision_PlayerActive.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class StateMachine_Decision_PlayerActive : StateMachine_Decision
+{
+    public override bool Decide(StateMachine_Controller _controller)
+    {
+        return _controller.chaseTarget.gameObject.activeSelf;
+    }
+}
